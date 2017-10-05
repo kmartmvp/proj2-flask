638e51a25923d84583b41ab203bcf0cc795b1afa
Michael Hagel <michaelhagel@Michaels-MacBook-Pro.local>
2017-10-05 12:58:50 -0700

    Added additional entry in pre.py that contains the date in MM/DD/YYYY format, relative to the beginning date. Changed syllabus.html to accomodate thi new entry in another column. Added an additional new entry to pre.py that determines if the week of the respective entry is the current week. New templating was used in syllabus.html to select a new css class that highlights the current week number if the aforementioned new entry returns True.

--- syllabus/pre.py
+++ syllabus/pre.py
@@ -9,6 +9,35 @@ logging.basicConfig(format='%(levelname)s:%(message)s',
 log = logging.getLogger(__name__)
 
 base = arrow.now()   # Default, replaced if file has 'begin: ...'
+current_date = arrow.now().format("MM/DD/YYYY")
+
+def current_week(date):
+    # Parse string date. Docs for 'get' method:
+    #   http://arrow.readthedocs.io/en/latest/
+    parsed_date = arrow.get(date, "MM/DD/YYYY")
+    today = arrow.now().format("MM/DD/YYYY")
+    parsed_today = arrow.get(today, "MM/DD/YYYY")
+
+    ### Logging ###
+    log.debug("Check: " + str(parsed_today))
+    log.debug("From: " + str(parsed_date))
+
+    # Shift days 6 to ensure we don't count beginning of next week
+    week_from_date = parsed_date.shift(days=6)
+
+    ### Logging ###
+    log.debug("Up until: " + str(week_from_date) + "\n")
+
+    date_range = []
+
+    for day in arrow.Arrow.range('day', parsed_date, week_from_date):
+        log.debug(str(day))
+        date_range.append(day)
+    
+    if(parsed_today in date_range):
+        return True
+    
+    return False
 
 
 def process(raw):
@@ -21,6 +50,8 @@ def process(raw):
     field = None
     entry = {}
     cooked = []
+    # Define count of weeks past base week (defined in syllabus passed through)
+    week_count = 0
     for line in raw:
         log.debug("Line: {}".format(line))
         line = line.strip()
@@ -51,7 +82,17 @@ def process(raw):
                 entry = {}
             entry['topic'] = ""
             entry['project'] = ""
+            # Shift the days to appropriate date for start date of week. Then,
+            #  format appropriately.
+            entry['date'] = base.shift(days = 7*week_count).format("MM/DD/YYYY")
+            
+            # Adds new entry for column that contains week number
             entry['week'] = content
+
+            entry['currentweek'] = current_week(entry['date'])
+            
+            # Increment week counter so we can correctly display dates
+            week_count += 1
 
         elif field == 'topic' or field == 'project':
             entry[field] = content
